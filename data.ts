84193d04f7b0e4eaa3bb7e4d3836ae6fcb9421ae
im4nu <victorx811@gmail.com>
2023-04-24 23:14:51 -0300

    changed the name of cards on data.ts

--- data.ts
+++ data.ts
@@ -22,7 +22,7 @@ export const LinkCards = [
     {
       logo: LinkedinLogo,
       image: LinkedinImage,
-      name: "Instagram",
+      name: "Linkedin",
       label: "@isabelly_estetica2022",
       bg: "linkedin-card",
       link: "https://www.linkedin.com/in/isabelly-freitas-189a4917b",
@@ -30,7 +30,7 @@ export const LinkCards = [
     {
       logo: TelegramLogo,
       image: TelegramImage,
-      name: "Instagram",
+      name: "Telegram",
       label: "@Isabellyfreitas",
       bg: "telegram-card",
       link: "https://t.me/+5588999617498",
@@ -38,7 +38,7 @@ export const LinkCards = [
     {
       logo: WhatsLogo,
       image: WhatsImage,
-      name: "Instagram",
+      name: "Whatsapp",
       label: "+55 88 9961-7498",
       bg: "whatsapp-card",
       link: "https://l.instagram.com/?u=https%3A%2F%2Fwa.me%2F5588999617498%3Ftext%3DBem%2Bvindo%2Bao%2BCentro%2Bde%2BEst%25C3%25A9tica%2BIsabelly%2BFreitas%2Baguarde%2Bque%2Bjaja%2Beu%2Bte%2Brespondo%2B%25F0%259F%2592%259C%25E2%2598%25BA&e=AT2O1lcI7MCpdoFrm-Fbh4h_S0VC0-0LI0M4Ks8Nk_BqxYbuXpOSHJSCwftuHtfpPrdiVXRs6W8MCv-vFdWx6QPBu37OHE23qSvursXrB7mBC8cLa_KsQg",
