7f7ea959abd95461f41d279f90d786d014e4f14d
im4nu <victorx811@gmail.com>
2023-04-24 23:15:25 -0300

    solved the whatsapp link

--- data.ts
+++ data.ts
@@ -41,7 +41,7 @@ export const LinkCards = [
       name: "Whatsapp",
       label: "+55 88 9961-7498",
       bg: "whatsapp-card",
-      link: "https://l.instagram.com/?u=https%3A%2F%2Fwa.me%2F5588999617498%3Ftext%3DBem%2Bvindo%2Bao%2BCentro%2Bde%2BEst%25C3%25A9tica%2BIsabelly%2BFreitas%2Baguarde%2Bque%2Bjaja%2Beu%2Bte%2Brespondo%2B%25F0%259F%2592%259C%25E2%2598%25BA&e=AT2O1lcI7MCpdoFrm-Fbh4h_S0VC0-0LI0M4Ks8Nk_BqxYbuXpOSHJSCwftuHtfpPrdiVXRs6W8MCv-vFdWx6QPBu37OHE23qSvursXrB7mBC8cLa_KsQg",
+      link: "wa.me%2F5588999617498%3Ftext%3DBem%2Bvindo%2Bao%2BCentro%2Bde%2BEst%25C3%25A9tica%2BIsabelly%2BFreitas%2Baguarde%2Bque%2Bjaja%2Beu%2Bte%2Brespondo%2B%25F0%259F%2592%259C%25E2%2598%25BA&e=AT2O1lcI7MCpdoFrm-Fbh4h_S0VC0-0LI0M4Ks8Nk_BqxYbuXpOSHJSCwftuHtfpPrdiVXRs6W8MCv-vFdWx6QPBu37OHE23qSvursXrB7mBC8cLa_KsQg",
     },
 ];
 
