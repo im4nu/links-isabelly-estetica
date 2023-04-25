dbb4bd475e9a3faf864535993b2c79be2406611d
im4nu <victorx811@gmail.com>
2023-04-24 23:07:16 -0300

    added the link of main button on services section

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,9 +64,12 @@ export default function Home() {
           sinta a diferença desde o primeiro momento.
         </p>
 
-        <button className="flex items-center bg-white text-main px-4 py-2 rounded-lg font-semibold">
+        <a
+          href="https://l.instagram.com/?u=https%3A%2F%2Fwa.me%2F5588999617498%3Ftext%3DBem%2Bvindo%2Bao%2BCentro%2Bde%2BEst%25C3%25A9tica%2BIsabelly%2BFreitas%2Baguarde%2Bque%2Bjaja%2Beu%2Bte%2Brespondo%2B%25F0%259F%2592%259C%25E2%2598%25BA&e=AT2O1lcI7MCpdoFrm-Fbh4h_S0VC0-0LI0M4Ks8Nk_BqxYbuXpOSHJSCwftuHtfpPrdiVXRs6W8MCv-vFdWx6QPBu37OHE23qSvursXrB7mBC8cLa_KsQg"
+          className="flex items-center bg-white text-main px-4 py-2 rounded-lg font-semibold"
+        >
           Agendar Agora
-        </button>
+        </a>
       </section>
 
       <section className="w-screen justify-center items-center py-20">
